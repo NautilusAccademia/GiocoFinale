b8fc2512d90af3efa0269765f084461e4a1ce893
elisarussonautilusacademy <61882353+elisarussonautilusacademy@users.noreply.github.com>
2020-04-21 11:29:43 +0200

    Sistemazione trappole
    + finti collezionabili

---
+++ Assets/Script/FalsePickup.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FalsePickup : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
