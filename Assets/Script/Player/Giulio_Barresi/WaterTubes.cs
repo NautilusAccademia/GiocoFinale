23f043417a5b45a6abae5c68618170714635a6c0
GiulioBarresi <61886410+GiulioBarresi@users.noreply.github.com>
2020-05-06 19:23:36 +0200

    L'acqua funziona
    Sappiamo come fare funzionare l'acqua con SetBlendShapeWeight
    
    Ma dobbiamo fare in modo che  aggiunge di 25 ad ogni bottone

--- Assets/Script/Player/Giulio_Barresi/WaterTubes.cs
+++ Assets/Script/Player/Giulio_Barresi/WaterTubes.cs
@@ -14,6 +14,8 @@ public class WaterTubes : MonoBehaviour
 
     bool buttonPressed;
 
+    float numWater = 0f;
+
     void Start()
     {
         buttonPressed = false;
@@ -27,7 +29,13 @@ public class WaterTubes : MonoBehaviour
         {
             buttonPressed = true;
             waterParticle.Play(true);
-            skinnedMeshRenderer.GetBlendShapeWeight(25);// fa alzare livello acqua 
+            //skinnedMeshRenderer.GetBlendShapeWeight(25);// fa alzare livello acqua
+        for(int i = 0; i < 4; i++)
+        {
+            skinnedMeshRenderer.SetBlendShapeWeight(0, (numWater += 25f));
         }
+            //skinnedMeshRenderer.SetBlendShapeWeight(0, (numWater += 25f));
+        }
+        
     }
 }
