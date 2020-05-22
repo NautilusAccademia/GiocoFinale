07a3ac840416c1be102465812b3d8f9a6cb45ce0
GiulioBarresi <61886410+GiulioBarresi@users.noreply.github.com>
2020-05-22 14:42:33 +0200

    Se cadi perdi una vita
    - Il player se cade nell'aria dove c'è l'acqua perde una vita e respawna all'inizio del livello.

---
+++ Assets/Script/Player/Giulio_Barresi/RespawnZone.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnZone : MonoBehaviour
+{
+    [SerializeField] Transform spawnPoint;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            GameManager.instance.playerGameObject.transform.position = spawnPoint.transform.position;
+            other.gameObject.GetComponent<Health>().DecreaseHealth();
+        }
+    }
+}
