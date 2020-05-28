0a9110ffcd0c63f598a61a8d16123c8340adf8bf
RossaCremisi <saradiraimondop@gmail.com>
2020-05-28 16:39:46 +0200

    Saving_Data
    Animazioni nuove inserite, save data health work in progress

--- Assets/Script/SaveSystem/ContinueButton.cs
+++ Assets/Script/SaveSystem/ContinueButton.cs
@@ -8,15 +8,21 @@ public class ContinueButton : MonoBehaviour
 {
     private int sceneToContinue;
 
+   
+
   public void ContinueGame()
     {
-        sceneToContinue = PlayerPrefs.GetInt("SavedScene");
+        sceneToContinue = PlayerPrefs.GetInt("SavedScene"); //apre i dati del salvataggio
+        
+
+
 
         if (sceneToContinue != 0)
         {
             SceneManager.LoadScene(sceneToContinue);
             Time.timeScale = 1f; //(Nanni) Whenever the start game button is pressed
                                  //The time begins to flow again instead of staying stopped
+
         }
 
         else
