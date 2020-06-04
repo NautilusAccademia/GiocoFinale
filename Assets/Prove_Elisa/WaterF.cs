1df4686dffc64a8e7b5a4ef686d3c49be038ab8f
elisarussonautilusacademy <61882353+elisarussonautilusacademy@users.noreply.github.com>
2020-06-04 21:33:23 +0200

    elisa acqua
    ennesima acquadimmerd

---
+++ Assets/Prove_Elisa/WaterF.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterF : MonoBehaviour
+{
+
+
+    public GameObject water;
+    SkinnedMeshRenderer skinnedMeshRenderer;
+    Mesh skinnedMesh;
+    float blendShape;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        skinnedMeshRenderer = water.GetComponent<SkinnedMeshRenderer>();
+        skinnedMesh = water.GetComponent<SkinnedMeshRenderer>().sharedMesh;
+        blendShape = 0;
+
+
+
+
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        skinnedMeshRenderer.SetBlendShapeWeight(0, blendShape);
+
+        if (other.gameObject.CompareTag("Air"))
+        {
+
+            blendShape += 25f;
+
+
+            // for (blendShape = 0f; blendShape <= 25f; blendShape++) ;
+
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+
+    }
+
+
+
+
+}
