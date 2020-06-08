314d3639e57b491722029ca8a2c208a3a58896fe
GiulioBarresi <61886410+GiulioBarresi@users.noreply.github.com>
2020-06-08 18:49:08 +0200

    fix bottoni

--- Assets/Prove_Elisa/WaterF.cs
+++ Assets/Prove_Elisa/WaterF.cs
@@ -4,48 +4,68 @@ using UnityEngine;
 
 public class WaterF : InteractableObjects
 {
-    [SerializeField] Conditions condition;
+    //[SerializeField] Conditions condition;
 
-    public GameObject water;
-    SkinnedMeshRenderer skinnedMeshRenderer;
-    Mesh skinnedMesh;
-    float blendShape;
-
-
-
+    //public GameObject water;
+    //SkinnedMeshRenderer skinnedMeshRenderer;
+    //Mesh skinnedMesh;
+    //public float blendShape;
 
     // Start is called before the first frame update
-    void Start()
+    /*void Start()
     {
         skinnedMeshRenderer = water.GetComponent<SkinnedMeshRenderer>();
         skinnedMesh = water.GetComponent<SkinnedMeshRenderer>().sharedMesh;
         blendShape = 0;
 
+        skinnedMeshRenderer.SetBlendShapeWeight(0, blendShape);
 
 
 
-    }
+    }*/
 
-
-    void OnTriggerEnter(Collider other)
+    /*private void Update()
     {
         skinnedMeshRenderer.SetBlendShapeWeight(0, blendShape);
+    }*/
+
+    /*void OnTriggerEnter(Collider other)
+    {
+        //skinnedMeshRenderer.SetBlendShapeWeight(0, blendShape);
 
         if (other.gameObject.CompareTag("Player") && condition.CheckCondition() && Input.GetKeyDown(KeyCode.E))
         {
-            ShootingElements.instance.ShootingAir();
-            blendShape += 25f;
+            
+            
+            
 
 
             // for (blendShape = 0f; blendShape <= 25f; blendShape++) ;
 
 
         }
+    }*/
+
+    void FillWater()
+    {
+        /*if (condition.CheckCondition() && Input.GetKeyDown(KeyCode.E))
+        {
+            
+            
+        }*/
+        ShootingElements.instance.ShootingAir();
+        //skinnedMeshRenderer.SetBlendShapeWeight(0, 25f);
+        //skinnedMeshRenderer.SetBlendShapeWeight(0, blendShape);
+
+        WaterShape.blendShape += 25f;
+       
+        
     }
 
     public override void SpecificInteraction()
     {
-        ShootingElements.instance.ShootingAir();
         base.SpecificInteraction();
+        FillWater();
+        ShootingElements.instance.ShootingAir();
     }
 }
---
+++ Assets/Script/WaterShape.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterShape : MonoBehaviour
+{
+    public GameObject water;
+    SkinnedMeshRenderer skinnedMeshRenderer;
+    Mesh skinnedMesh;
+    public static float blendShape;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        skinnedMeshRenderer = water.GetComponent<SkinnedMeshRenderer>();
+        skinnedMesh = water.GetComponent<SkinnedMeshRenderer>().sharedMesh;
+        blendShape = 0;
+
+        skinnedMeshRenderer.SetBlendShapeWeight(0, blendShape);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        skinnedMeshRenderer.SetBlendShapeWeight(0, blendShape);
+    }
+}
