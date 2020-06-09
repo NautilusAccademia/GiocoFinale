2b4c2880fe682554ae87856c01daf53a243d2719
Nanni-clr <61884961+Nanni-clr@users.noreply.github.com>
2020-06-09 18:18:11 +0200

    Exit bottone
    Se il bottone Exit del menu principale viene schiacciato durante la build, il gioco si chiuderà

---
+++ Assets/Script/ExitGame.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitGame : MonoBehaviour
+{
+    public void QuitGame()//method that quits the game
+    {
+        Application.Quit();//Quit the game
+    }
+}
