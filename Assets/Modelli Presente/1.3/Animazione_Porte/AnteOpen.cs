4c30a9e5819323864b5b1fc5e1f19676f5528505
RossaCremisi <saradiraimondop@gmail.com>
2020-06-12 12:11:18 +0200

    Porta1.3
    Si apre con l'aria

---
+++ Assets/Modelli Presente/1.3/Animazione_Porte/AnteOpen.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnteOpen : InteractableObjects
+{
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+   private void OpenAnte()
+    {
+        ShootingElements.instance.ShootingAir();
+
+        anim.SetTrigger("Open_Ante");
+        anim.SetTrigger("Open_Ante2");
+    }
+
+    public override void SpecificInteraction()
+    {
+        base.SpecificInteraction();
+        OpenAnte();
+        ShootingElements.instance.ShootingAir();
+    }
+}
